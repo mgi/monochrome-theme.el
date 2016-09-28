2f7f0620eefc7a221e107678d23d406560f3f5c8
Manuel Giraud <manuel@ledu-giraud.fr>
2016-09-28 16:30:08 +0200

    remove upper contrast in order to darken the default. highlight and string only.

--- monochrome-theme.el
+++ monochrome-theme.el
@@ -1,4 +1,4 @@
-;;; monochrome-theme.el --- A dark Emacs 24 theme for your focused hacking sessions
+;;; monochrome-theme.el --- A monochrome emacs theme for your focused hacking sessions
 
 ;; Copyright (C) 2011 Xavier Noria
 ;;
@@ -7,17 +7,15 @@
 ;;
 ;; This theme is based on the builtin dichromacy theme.
 ;;
-;; Works with Emacs 24.
+;; Works with Emacs 25.
 
 (deftheme monochrome
   "Gray on black for your focused hacking sessions.")
 
-(defun monochrome-set (cursor bg fg gg hg ig eg dg)
+(defun monochrome-set (cursor bg fg gg eg dg)
   "bg is background.
    fg is foreground.
    gg is an fg with more contrast.
-   hg is an gg with more contrast.
-   ig is an hg with more contrast.
    eg is an fg with less contrast.
    dg is an eg with less contrast."
 
@@ -31,36 +29,36 @@
 
      ;; Highlighting faces
      `(fringe ((,class (:background ,bg))))
-     `(highlight ((,class (:foreground ,bg :background ,gg))))
-     `(region ((,class (:foreground ,bg :background ,gg))))
-     `(secondary-selection ((,class (:foreground: ,bg :background ,hg))))
-     `(isearch ((,class (:foreground ,bg :background ,gg))))
-     `(lazy-highlight ((,class (:foreground ,bg :background ,gg))))
-     `(trailing-whitespace ((,class (:background "red"))))
-     `(show-paren-match ((,class (:weight bold :foreground ,gg))))
-     `(show-paren-mismatch ((,class (:foreground ,gg :background "dark olive green"))))
+     `(highlight ((,class (:foreground ,bg :background ,fg))))
+     `(region ((,class (:foreground ,bg :background ,fg))))
+     `(secondary-selection ((,class (:foreground: ,bg :background ,fg))))
+     `(isearch ((,class (:foreground ,bg :background ,fg))))
+     `(lazy-highlight ((,class (:foreground ,bg :background ,fg))))
+     `(trailing-whitespace ((,class (:background ,fg))))
+     `(show-paren-match ((,class (:weight bold :foreground ,fg))))
+     `(show-paren-mismatch ((,class (:foreground ,bg :background ,fg))))
 
      ;; Mode line faces
-     `(mode-line ((,class (:background ,gg :foreground ,bg))))
-     `(mode-line-inactive ((,class (:background ,fg :foreground ,bg))))
+     `(mode-line ((,class (:background ,fg :foreground ,bg))))
+     `(mode-line-inactive ((,class (:background ,eg :foreground ,bg))))
 
      ;; Escape and prompt faces
      `(minibuffer-prompt ((,class (:weight bold :foreground ,gg))))
      `(escape-glyph ((,class (:foreground ,gg))))
-     `(error ((,class (:weight bold :slant italic :foreground "firebrick"))))
+     `(error ((,class (:weight bold :foreground "firebrick"))))
      `(warning ((,class (:foreground "dark orange"))))
      `(success ((,class (:foreground "dark green"))))
 
      ;; Font lock faces
-     `(font-lock-builtin-face ((,class (:foreground ,gg))))
-     `(font-lock-comment-face ((,class (:slant italic :foreground ,fg))))
+     `(font-lock-builtin-face ((,class (:foreground ,fg))))
+     `(font-lock-comment-face ((,class (:weight bold :foreground ,gg))))
      `(font-lock-constant-face ((,class (:weight bold :foreground ,gg))))
-     `(font-lock-function-name-face ((,class (:foreground ,ig))))
-     `(font-lock-keyword-face ((,class (:weight bold :foreground ,gg))))
-     `(font-lock-string-face ((,class (:foreground ,hg))))
-     `(font-lock-type-face ((,class (:weight bold :foreground ,ig))))
-     `(font-lock-variable-name-face ((,class (:weight bold :foreground ,hg))))
-     `(font-lock-warning-face ((,class (:weight bold :foreground ,ig))))
+     `(font-lock-function-name-face ((,class (:foreground ,fg))))
+     `(font-lock-keyword-face ((,class (:foreground ,fg))))
+     `(font-lock-string-face ((,class (:foreground ,gg))))
+     `(font-lock-type-face ((,class (:foreground ,fg))))
+     `(font-lock-variable-name-face ((,class (:weight bold :foreground ,fg))))
+     `(font-lock-warning-face ((,class (:weight bold :foreground ,fg))))
 
      ;; Info
      `(info-menu-header ((,class (:weight bold :height 1.1))))
@@ -69,8 +67,8 @@
      `(info-title-3 ((,class (:weight bold :height 1.2))))
      `(info-title-4 ((,class (:weight bold))))
      `(info-header-node ((,class (:foreground ,gg))))
-     `(info-header-xref ((,class (:foreground ,hg))))
-     `(info-node ((,class (:foreground ,ig))))
+     `(info-header-xref ((,class (:foreground ,fg))))
+     `(info-node ((,class (:foreground ,fg))))
 
      ;; Button and link faces
      `(link ((,class (:underline t :foreground ,gg))))
@@ -79,15 +77,15 @@
      ;; Speedbar
      `(speedbar-button-face ((,class (:foreground ,fg))))
      `(speedbar-file-face ((,class (:foreground ,gg))))
-     `(speedbar-directory-face ((,class (:weight bold :foreground ,ig))))
+     `(speedbar-directory-face ((,class (:weight bold :foreground ,fg))))
      `(speedbar-tag-face ((,class (:foreground ,fg))))
      `(speedbar-selected-face ((,class (:underline ,gg :foreground ,gg))))
-     `(speedbar-highlight-face ((,class (:weight bold :background ,bg :foreground ,ig))))
+     `(speedbar-highlight-face ((,class (:weight bold :background ,bg :foreground ,fg))))
 
      ;; ido
      `(ido-first-match ((,class (:foreground ,gg))))
      `(ido-only-match ((,class (:underline ,gg :foreground ,gg))))
-     `(ido-subdir ((,class (:weight bold :foreground ,ig))))
+     `(ido-subdir ((,class (:weight bold :foreground ,fg))))
 
      ;; MuMaMo
      `(mumamo-background-chunk-major ((,class (:background ,bg))))
@@ -96,9 +94,9 @@
      `(mumamo-background-chunk-submode3 ((,class (:background ,bg))))
      `(mumamo-background-chunk-submode4 ((,class (:background ,bg))))
      `(mumamo-border-face-in ((,class (:slant unspecified :underline unspecified
-					      :weight bold :foreground ,ig))))
+					      :weight bold :foreground ,fg))))
      `(mumamo-border-face-out ((,class (:slant unspecified :underline unspecified
-					       :weight bold :foreground ,ig))))
+					       :weight bold :foreground ,fg))))
 
      ;; Gnus faces
      `(gnus-group-news-1 ((,class (:weight bold :foreground ,gg))))
@@ -136,17 +134,17 @@
      `(gnus-header-newsgroups ((,class (:foreground ,gg))))
      `(gnus-summary-high-ancient ((,class (:foreground ,fg))))
      `(gnus-summary-high-read ((,class (:foreground ,fg))))
-     `(gnus-summary-high-ticked ((,class (:foreground ,ig))))
+     `(gnus-summary-high-ticked ((,class (:foreground ,fg))))
      `(gnus-summary-high-undownloaded ((,class (:foreground ,fg :strike-through t))))
      `(gnus-summary-high-unread ((,class (:foreground ,gg))))
      `(gnus-summary-low-ancient ((,class (:foreground ,fg))))
      `(gnus-summary-low-read ((,class (:foreground ,fg))))
-     `(gnus-summary-low-ticked ((,class (:foreground ,ig))))
+     `(gnus-summary-low-ticked ((,class (:foreground ,fg))))
      `(gnus-summary-low-undownloaded ((,class (:foreground ,fg :strike-through t))))
      `(gnus-summary-low-unread ((,class (:foreground ,gg))))
      `(gnus-summary-normal-ancient ((,class (:foreground ,fg))))
      `(gnus-summary-normal-read ((,class (:foreground ,fg))))
-     `(gnus-summary-normal-ticked ((,class (:foreground ,ig))))
+     `(gnus-summary-normal-ticked ((,class (:foreground ,fg))))
      `(gnus-summary-normal-undownloaded ((,class (:foreground ,fg :strike-through t))))
      `(gnus-summary-normal-unread ((,class (:foreground ,gg))))
      `(gnus-summary-selected ((,class (:foreground ,gg :bold t))))
@@ -171,39 +169,39 @@
      `(message-header-subject ((,class (:foreground ,gg))))
      `(message-header-to ((,class (:weight bold :foreground ,gg))))
      `(message-header-xheader ((,class (:foreground ,gg))))
-     `(message-header-newsgroups ((,class (:weight bold :foreground ,ig))))
+     `(message-header-newsgroups ((,class (:weight bold :foreground ,fg))))
      `(message-cited-text ((,class (:slant italic :foreground ,gg))))
-     `(message-mml ((,class (:foreground ,hg))))
+     `(message-mml ((,class (:foreground ,fg))))
      `(message-separator ((,class (:weight bold :foreground ,gg))))
 
      ;; w3m
      `(w3m-anchor ((,class (:inherit link))))
      `(w3m-arrived-anchor ((,class (:inherit link-visited))))
      `(w3m-form ((,class (:foreground ,gg))))
-     `(w3m-header-line-location-title ((,class (:foreground ,hg))))
+     `(w3m-header-line-location-title ((,class (:foreground ,fg))))
      `(w3m-header-line-location-content ((,class (:foreground ,gg))))
      `(w3m-bold ((,class (:weight bold))))
-     `(w3m-image-anchor ((,class (:foreground ,hg :inherit link))))
-     `(w3m-image ((,class (:foreground ,hg))))
+     `(w3m-image-anchor ((,class (:foreground ,fg :inherit link))))
+     `(w3m-image ((,class (:foreground ,fg))))
      `(w3m-session-select ((,class (:foreground ,gg))))
      `(w3m-session-selected ((,class (:foreground ,gg :underline t))))
      `(w3m-tab-background ((,class (:background ,bg :foreground ,bg))))
      `(w3m-tab-selected-background ((,class (:background ,bg :foreground ,bg))))
      `(w3m-tab-mouse ((,class (:background ,fg :foreground "yellow"))))
-     `(w3m-tab-selected ((,class (:background ,fg :foreground ,ig :bold t))))
-     `(w3m-tab-unselected ((,class (:background ,fg :foreground ,gg))))
-     `(w3m-tab-selected-retrieving ((,class (:background ,fg :foreground ,ig :bold t))))
-     `(w3m-tab-unselected-retrieving ((,class (:background ,fg :foreground ,ig))))
-     `(w3m-tab-unselected-unseen ((,class (:background ,fg :foreground ,ig))))
+     `(w3m-tab-selected ((,class (:background ,fg :foreground ,bg :bold t))))
+     `(w3m-tab-unselected ((,class (:background ,bg :foreground ,fg))))
+     `(w3m-tab-selected-retrieving ((,class (:background ,fg :foreground ,bg :bold t))))
+     `(w3m-tab-unselected-retrieving ((,class (:background ,bg :foreground ,fg))))
+     `(w3m-tab-unselected-unseen ((,class (:background ,bg :foreground ,fg))))
 
      ;; Org-mode
      `(org-agenda-structure ((,class (:foreground ,fg :underline t))))
      `(org-agenda-date ((,class (:foreground ,gg :underline nil))))
-     `(org-agenda-date-today ((,class (:foreground ,hg :bold t :underline nil))))
+     `(org-agenda-date-today ((,class (:foreground ,fg :bold t :underline nil))))
      `(org-time-grid ((,class (:foreground ,fg))))
 
      ;; EShell
-     `(eshell-prompt ((,class (:foreground ,ig :bold t))))
+     `(eshell-prompt ((,class (:foreground ,fg :bold t))))
      `(eshell-ls-archive ((,class (:inherit eshell-ls-unreadable))))
      `(eshell-ls-backup ((,class (:inherit eshell-ls-unreadable))))
      `(eshell-ls-clutter ((,class (:inherit eshell-ls-unreadable))))
@@ -220,8 +218,8 @@
      `(sh-quoted-exec ((,class (:inherit font-lock-string-face))))
 
      ;; Slime
-     `(slime-repl-inputed-output-face ((,class (:foreground ,hg))))
-     `(slime-repl-output-face ((,class (:weight bold :foreground ,hg))))
+     `(slime-repl-inputed-output-face ((,class (:foreground ,fg))))
+     `(slime-repl-output-face ((,class (:weight bold :foreground ,fg))))
 
      ;; Customize
      `(custom-variable-tag ((,class (:weight bold :foreground ,gg))))
@@ -232,19 +230,15 @@
      `(flyspell-duplicate ((,class (:weight unspecified :foreground unspecified
 					    :slant unspecified :underline ,gg))))
      `(flyspell-incorrect ((,class (:weight unspecified :foreground unspecified
-					    :slant unspecified :underline ,gg)))))
-
-    (custom-theme-set-variables
-     'monochrome
-     `(ansi-color-names-vector [,bg ,gg ,fg ,hg]))))
+					    :slant unspecified :underline ,gg)))))))
 
 (defun monochrome-dark ()
   (interactive)
-  (monochrome-set "goldenrod" "black" "gray50" "gray60" "gray70" "white" "gray40" "gray30"))
+  (monochrome-set "goldenrod" "black" "gray70" "white" "gray40" "gray30"))
 
 (defun monochrome-bright ()
   (interactive)
-  (monochrome-set "purple" "white" "gray30" "gray20" "gray10" "black" "gray40" "gray50"))
+  (monochrome-set "purple" "white" "gray20" "black" "gray40" "gray50"))
 
 (defun monochrome-toggle ()
   "Toggle between bright/dark version of the theme."
